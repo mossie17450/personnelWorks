fe6e5ad4f68618b8142142f9b6c02b4fdcf31f06
mossie17450 <mossie17450@gmail.com>
2018-10-26 11:16:26 +0200

    Add files via upload

--- html/missions.php
+++ html/missions.php
@@ -1,43 +1,32 @@
 <?php
  session_start();
- //session_register(); // à tester
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr" dir="ltr">
+<!DOCTYPE html><!-- html5-->
+<!--<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr" dir="ltr">-->
 <head>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108361116-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-108361116-1');
-</script>
-
-<!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
-
-<meta http-equiv="Content-Type" content="text/html; charset=utf8" />
-  <title>Innovation biomédicale, recherche de biomateriaux avec le CHU de Bordeaux : le CIC-IT. Aquitaine - France</title>
+<title>Innovation biomédicale, recherche de biomateriaux avec le CHU de Bordeaux : le CIC-IT. Aquitaine - France</title>
+<meta name="robots" content="noimageindex" /><!-- je ne met pas les images du site à l'indexation par les robots -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="CIC, centre, investigation, clinique, innovation, technologique, biomatériaux, bordeaux, CIC-IT, chu bordeaux, PTIB, Master, bidim, cellules, in vivo, in vitro, médical, recherche, Pessac, Arnozan, hôpital, réparation tissulaire, CIC IT, biomédical, Aquitaine, ingénierie tissulaire, scientifique" />
-<meta name="description" content="Acteur aquitain de l&#039;innovation biomédicale, le Centre d&#039;Investigation Clinique Innovation Technologique Biomatériaux de Bordeaux opère au sein d&#039;une structure scientifique actuelle et avancée pour appuyer tous ces travaux de recherche." />
+<meta name="description" content="Acteur aquitain de l'innovation biomédicale, le Centre d'Investigation Clinique Innovation Technologique Biomatériaux de Bordeaux opère au sein d&#039;une structure scientifique actuelle et avancée pour appuyer tous ces travaux de recherche." />
 <!-- icon de la barre du navigateur. -->
 <link rel="icon" type="image/png" href="../image/logos/logoCIC-ITB.png" />
 <!--[if IE]><link rel="icon" type="image/png" href="../image/logos/logoCIC-ITB.png" /><![endif]-->
 
-<!--- ma premiere( presque) feuille de style : -->
-<!--<link rel="stylesheet" type="text/css" href="../css/SecondePage.css"> -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">	
-<!--<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">-->
-<!--<link rel="stylesheet" type="text/css" href="../css/SecondePage.css" />-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+   
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">	
+-->
+<!--<link rel="stylesheet"  type="text/css"   media="screen and (min-width:300px)"  href="css/CSSPage.css" /> <!-- a corriger avec un autre fichier .css (min-width:240 and max-width:640px). celui-ci devient :min-width:640px   --> 
+<link rel="stylesheet"  media="min-width:100px" media="max-width:1200px" href="../css/CSSPage.css" />
+<link rel="stylesheet"  type="text/css"   media="screen and (min-width:100px)"  href="../css/menuHD.css" />
+<link rel="stylesheet" type="text/css" media="screen and (min-width:100px)" href="../css/menuHDR2.css" />
+<!--<link rel="stylesheet" type="text/css" media="screen and (max-width:300px)"  media="screen and (min-width:100px)" href="css/menuHDR2petit.css" />-->
 
-<link rel="stylesheet" type="text/css" href="../css/menuHD.css" />
-
-<link rel="stylesheet" type="text/css"  media="screen and (min-width:240px)"href="../css/CSSPage.css" />
-<!--<link rel="stylesheet" media="screen and (max-width:640px)" href="../css/toutpetitResolution.css" />-->
-
-<!--<link rel="stylesheet" type="text/css" media="screen and (max-width:640px)" href="../css/menuHDR2petit.css" />-->
-<link rel="stylesheet" type="text/css" media="screen and (min-width:240px)" href="../css/menuHDR2.css" />
 
 <script type="text/javascript">
 <!--
@@ -52,171 +41,97 @@ if (d) {d.style.display='block';}
 
 //-->
 </script>
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    /* fonction demander par la CNIL  */
+    _paq.push([function() { 
+var self = this; 
+function getOriginalVisitorCookieTimeout() { 
+var now = new Date(), 
+nowTs = Math.round(now.getTime() / 1000), 
+visitorInfo = self.getVisitorInfo(); 
+var createTs = parseInt(visitorInfo[2]); 
+var cookieTimeout = 33696000; // 13 mois en secondes 
+var originalTimeout = createTs + cookieTimeout -
+nowTs; 
+return originalTimeout; 
+} 
+this.setVisitorCookieTimeout( getOriginalVisitorCookieTimeout() ); 
+}]);
+   /* fin de la fonction demander par la CNIL  */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//www.cic-it-bordeaux.fr/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
 </head>
-<body style="font-family:'Century Gothic Normal'; height:auto; " >
- <?php
-	  if($_SESSION['lang'] && empty($_GET['langue'])){
- ($_SESSION['lang']);
- }
-	else if(!empty($_GET['langue']) ){
- $_SESSION['lang']=$_GET['langue']; //si $_SESSION[lang] OK ne rien change
- }
+<!-- langue par défaut                        -->
+<?php include'../inc/languesParDefauthtml.php';?>
+<body style="font-family:'Century Gothic Normal';  max-width:1500px; ">
 
- //inclusion des fichiers langue ... différent du choix de la langue.
-	if ($_SESSION['lang']=='fr') {           // si la langue est 'fr' (français) on inclut le fichier fr-lang.php
- include('../lang/fr-lang.php');
-  	 } 
-  	 
-  	 else if ($_SESSION['lang']=='en') {      // si la langue est 'en' (anglais) on inclut le fichier en-lang.php
-  	 include('../lang/en-lang.php');
-  	 }
-  	 
-  	 else {                       // si aucune langue n'est déclarée on inclut le fichier fr-lang.php par défaut
-  	 include('../lang/fr-lang.php');
-  	 }
-	 ?>
-<div id="body" class="arrondie">
-<!-- entete -->
-
-<div id="header" style=" display:flex; flex-direction:row; height:auto; " >
-<div style="display:flex; width:10%;"><a href="https://dev.cic-it-bordeaux.fr/index.php" style="float:left; "><img src="../image/logos/CIC-ITN.jpg" class="arrondie" style="width:100%; height;auto;"></a></div>
-<div style="display:flex;  width:80%; flex-direction:column;">
-
-<img src="../image/logos/CICITBordeaux.png"  class="arrondie" style="width:100%;" >
-</div>
-<div style="display:flex; width:10%;"><a href="http://www.cic-it.fr/index.php" style="float:right;"><img src="../image/logos/reseauCIC-ITN.jpg"  class="arrondie" style="width:100%; height;auto;" ></a></div>
-</div>
-
-<br>
- <div  style="display:flex; flex-direction:row;"> 
-<div id="contenu" align="inline" width="100%">
-
-<!-- formulaire de choix des langue :icon auto-submit -->
-<div id="CdL" style="float:right; align:inline; "> 
-
-<!-- nouvel mise en forme des "choix de langues"  -->
-
-<?php
-		if (preg_match('~MSIE|Internet Explorer~i', $_SERVER['HTTP_USER_AGENT']) || (strpos($_SERVER['HTTP_USER_AGENT'], 'Trident/7.0; rv:11.0') !== false) || (strpos($_SERVER['HTTP_USER_AGENT'], 'windows phone') !== false)) {
-			echo "
-<div style=\"display:flex; flex-direction:row; float:left; width:200% \">			
-			<form  action=\"\" methode=\"post\" name=\"automatiquement\" id=\"choixlangue\" style=\"  \">
-<div  class=\"table-responsive, ex2\" ><table style=\"border:none; width:30%; float:left; \"><tr colspan=\"12\">
-
-<td colspan=\"1\"><img src=\"../image/transparent.png\" width=\"5%\" ></td>	
-<td colspan=\"4\"><a href=\"#\" style=\"display:flex; flex-direction:row;\"><input type=\"radio\"  name=\"langue\" id=\"choixlangue1\"  onclick=\"this.form.submit();\" value=\"fr\"><label><img src=\"../image/fr.png\" width=\"100%\"></a></td>
-<td colspan=\"1\"><img src=\"../image/transparent.png\" width=\"5%\" ></td>	
-<td colspan=\"4\"><a href=\"#\" style=\"display:flex; flex-direction:row;\"><input type=\"radio\"  name=\"langue\" id=\"choixlangue2\"  onclick=\"this.form.submit();\" value=\"en\"><label><img src=\"../image/en.png\" width=\"100%\"></a></td>
-<td colspan=\"2\"><a href=\"html/admin.php\" ><label><img src=\"../image/cadenasp.png\" width=\"100%\"></a></td>
-</tr></table></div>	
-			"; 	
-		}
-		
-		else {			
-		echo "
-		<form  action=\"\" methode=\"post\" name=\"automatiquement\" id=\"choixlangue\" >
-		<div class=\"table-responsive, ex2\" style=\"min-height:50px; min-width:100px;\">			
-			<table class=\"table, ex2\" style=\"border:none; min-height:50px; min-width:100px;\"><tr colspan=\"13\">			
-<a href=\"\" ><input type=\"radio\" hidden name=\"langue\" id=\"choixlangue1\" onclick=\"this.form.submit();\" value=\"fr\"><td colspan=\"4\"><label for=\"choixlangue1\"><img src=\"../image/fr.png\" width=\"100%\"></td></a><td colspan=\"1\"><img src=\"../image/transparent.png\" width=\"10%\" height=\"auto\"></td>
-<a href=\"\"><input type=\"radio\" hidden  name=\"langue\" id=\"choixlangue2\" onclick=\"this.form.submit();\" value=\"en\"><td colspan=\"4\"><label for=\"choixlangue2\"><img src=\"../image/en.png\" width=\"100%\"></td></a><td colspan=\"1\"img src=\"../image/transparent.png\" width=\"10%\" height=\"auto\"></td>		
-	<td colspan=\"3\"><a href=\"admin.php\" ><img src=\"../image/cadenasp.png\" width=\"100%\"></a></td>
-	</tr></table></div>
-		";
-		}		
-		?>	
-</form>		
- </div>
-
-<?php
-
- if($_SESSION['lang'] && empty($_POST['langue'])){
- $_SESSION['lang'];
- }
-else if($_POST['langue']=='fr'){
- include('lang/fr-lang.php');
-$_SESSION['lang']='fr';
-
-}
-else if($_POST['langue']=='en'){
-
- $_SESSION['lang']='en';
- include('lang/en-lang.php');
-}
-//echo "langue:".$_SESSION['lang'];
-?>
-
- 
- </div>
- </div></div>
+<div>
+<!-- entete                                    -->
 <!--                                           -->
-<!-- choix des langues, fin -->
+<!-- choix des langues                         -->
 <!--                                           -->	
-<!-- menu HDR(Horizontale Deroulant Responsif, boostrap) -->  
-<br>
- <div  style="display:flex; flex-direction:row;"> 
-<div id="contenu" align="inline" width="100%">     
+<!-- menu HDR2                                 -->  
+<?php include "../inc/EnTeteCICITBordeauxhtml.php";?>
+<?php include '../inc/choixDesLangueshtml.php';?>
+<?php include '../inc/objetMenuhtml.php'; ?>
 
-<nav>
-<ul id="menu" style="display:inline;"><?php echo TXT_Menuhtml; ?>
-</ul>
-</nav>
+<!-- corps de la page  --> 
+<div id="contenu" style="display:flex; flex-direction:row; align:inline-block;" >
+<evaluation clinique et preclinique >
 
-</div>
-</div>
-  <!-- corps de la page  -->
-  
-<div  style="display:flex; flex-direction:row;" >
-<div id="contenu" align="inline-block" >
-
-<div style="margin-left:5%; margin-right:5%; align:justfy;">
-<!--<h1><?php //echo TXT_TitreMission; ?></h1>-->
+<div style="align:justfy;">
 
 </br>
-<h2><?php echo TXT_SSSTitreMission; ?></h2>
-<h6><?php echo "<br>".TXT_SSMenuMission; ?></h6>		
-<br><h6><?php echo "<br>".TXT_ArticleMission; ?></h6>
-</div>
+<h2 align="center"><?php echo TXT_SSSTitreMission; ?></h2>
+<div style="text-align:justify; margin-left:2%; margin-right:2%;"><h4><?php echo "<br>".TXT_SSMenuMission; ?></h4>		
+<br><h4><?php echo "<br>".TXT_ArticleMission; ?></h4>
+</div></div>
 
- <div  class="table-responsive, ex2">
-<table class="table, ex2" width="100%"><tr colspan="16">
-<td><img src="../image/markerTransparent.png" width="1%" height="auto"></td>
-<td colspan="2"><a href="http://www.cic-it.fr/" ><img src="../image/logos/reseauCIC-ITter.jpg" width="100%"></a></td>
-  <td><img src="../image/markerTransparent.png" width="1%" height="auto"></td>
-  <td colspan="2"><a href="http://ast-innovations.com/"><img src="../image/Satt-Aquitaine.png" width="100%" ></a></td>
-  <td><img src="../image/markerTransparent.png" width="1%" height="auto"></td>
-  <td colspan="3"><a href="http://euclid-ctu.eu/"><img src="../image/logos/logo-euclid.jpg" width="100%" ></a></td>
-  <td><img src="../image/markerTransparent.png" width="1%" height="auto"></td>
-  <td colspan="1"><a href="http://www.fcrin.org/" ><img src="../image/logos/F-CRIN.png" width="100%" ></a></td> 
- <td><img src="../image/markerTransparent.png" width="1%" height="auto"></td>
-    <td colspan="2"><a href="http://tech4health.fr/" ><img src="../image/logos/tech4health.jpg" width="100%" ></a></td>
- <td><img src="../image/markerTransparent.png" width="1%" height="auto"></td>	
-  </tr>
- </table>
-</div>
+<div class="blink_meb">
+<div class="container-fluid, blink_meb"><div class="blink_meb row" >
 
+<div class="col-4  col-sm-3" ><a href="http://www.cic-it.fr/"><img src="../image/logos/reseauCIC-ITter.jpg" style="margin-left:25%; width:65%; margin-top:5%;" class="blink_meb"></a></div>
+<div class="col-4  col-sm-2" ><a href="http://ast-innovations.com/"><img src="../image/Satt-Aquitaine.png" style="margin-left:5%; width:65%; margin-top:5%;" ></a></div>
+   <!-- correction de l'adresse d'euclide....   -->
+<div class="col-4  col-sm-2" ><a href="https://ssl3.isped.u-bordeaux2.fr/euclid-st/Default.aspx?tabid=57"><img src="../image/logos/logo-euclid.jpg" style="margin-left:5%; width:80%; margin-top:5%;" ></a></div>
+<div class="col-4 col-sm-2" ><a href="http://www.fcrin.org/" class="blink_meb"><img src="../image/logos/F-CRIN.png" style="margin-left:5%; width:30%; margin-top:5%;" ></a></div> 
+<div  class="col-4  col-sm-3" ><a href="http://tech4health.fr/" ><img src="../image/logos/tech4health.jpg" style="margin-left:5%; width:65%; margin-top:5%;" class="blink_meb" ></a></div>
+
+</div></div>
+<!--
+echo"<div class=\"container-fluid\"> 
+  <div class=\"row\">
+    <div class=\"col-6 col-sm-3\" /><a href=\"https://www.chu-bordeaux.fr\" ><img src=\"../image/logos/CHUBordeaux.jpg\"  style=\"margin-left:25%; width:50%; margin-top:5%;\" ></a></div>
+    <div class=\"col-6 col-sm-3\" /><a href=\"http://www.aquitaine-poitou-charentes.inserm.fr\"><img src=\"../image/logos/inserm_logo_b.png\" style=\"margin-left:25%; width:50%; margin-top:15%; \"></a></div>
+    <div class=\"col-6 col-sm-3\" /><a href=\"https://www.u-bordeaux.fr\"><img src=\"../image/logos/universitebN.jpg\" style=\"margin-top:0%; margin-left:25%; width:50%;\"></div>
+    <div class=\"col-6 col-sm-3\" /><a href=\"http://www.bergonie.org\" ><img src=\"../image/logos/BergonieN.jpg\"  style=\"margin-top:10%; margin-left:30%; width:30%;\" ></a></div>
+  </div>
+ class="blink_meb" 
 </div>
+";
+-->
+</evaluation clinique et preclinique>
 </div>
- <br>
- <!-- pied de page  -->
-<div style="display:flex; flex-direction:column; ">
-<div  class="table-responsive, ex2" >
-<table class="table, ex2" width="100%"><tr colspan="10">
- <td><img src="../image/markerTransparent.png" width="5%" height="auto"></td>
-<td colspan="1"><a href="https://www.chu-bordeaux.fr" ><img src="../image/logos/ChuBordeauxN.jpg" width="50%" class="displayed"></a></td>
-  <td><img src="../image/markerTransparent.png" width="5%" height="auto"></td>
-  <td colspan="2"><a href="http://www.aquitaine-poitou-charentes.inserm.fr"><img src="../image/logos/INSERMN.jpg" width="50%" class="displayed"></a></td>
-  <td><img src="../image/markerTransparent.png" width="5%" height="auto"></td>
-  <td colspan="2"><a href="http//www.u-bordeaux.fr"><img src="../image/logos/universitebN.jpg" width="50%" class="displayed"></a></td>
-  <td><img src="../image/markerTransparent.png" width="5%" height="auto"></td>
-  <td><a href="http://www.bergonie.org" ><img src="../image/logos/BergonieN.jpg" width="70%" class="displayed"></a></td> 
-   <td><img src="../image/markerTransparent.png" width="5%" height="auto"></td>
-  </tr>
- </table>
+<!-- corps de la page, fin  -->
+<br>
+ 
+<!-- pied de page  -->
+<div style="display:flex; flex-direction:column; "> 
+<?php include '../inc/piedDePage.php'; ?> 
 </div>
- <div id="test" class="arrondie">
-  <?php echo TXT_Piedhtml ; ?>
-  </div></div>
- <!-- fin ajout -->
+<!-- pied de page :fin  -->
  </div>
 </body>
 </html>
